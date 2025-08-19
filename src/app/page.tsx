4caf914811ce0d624fd2cf31df3c0878ce712a80
Guilherme Faccin <gfaccin27@gmail.com>
2025-08-18 21:36:13 -0300

    feat: created hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Header from "@/components/header";
+import Hero from "@/components/hero";
 
 export default function Home() {
   return (
     <div className="bg-[#19191c] text-gray-200 min-h-screen font-aeonik">
       <Header />
+      <Hero />
     </div>
   );
 }
